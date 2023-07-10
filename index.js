63c9c5b90f570dc252eb0406115b08bcc35449e3
Sebastian Brown <seb.brown95@gmail.com>
2023-07-11 09:51:28 +1000

    wip styling and color easter egg

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 let currentItem = "about";
 
-function onScrolledAway()
-
-function createObserver(querySelector) {
+function createObserver(querySelector, atTop, notAtTop) {
   // Based on: https://css-tricks.com/styling-based-on-scroll-position/
   if (
     "IntersectionObserver" in window &&
@@ -12,13 +10,90 @@ function createObserver(querySelector) {
     let observer = new IntersectionObserver((entries) => {
       if (entries[0].boundingClientRect.y < 0) {
         console.log("not at top", querySelector);
+        notAtTop();
       } else {
         console.log("at top", querySelector);
+        atTop();
       }
     });
     observer.observe(document.querySelector(querySelector));
   }
 }
 
-createObserver("#about");
-createObserver("#skills");
+function updateNav() {
+  switch (currentItem) {
+    case "about":
+      const about = document.querySelector("#about");
+      about.classList.add("selected");
+      break;
+
+    default:
+      break;
+  }
+}
+
+createObserver(
+  "#about",
+  () => {
+    currentItem = "about";
+    updateNav();
+  },
+  () => {}
+);
+createObserver(
+  "#skills",
+  () => {
+    currentItem = "skills";
+    updateNav();
+  },
+  () => {}
+);
+
+
+currentColor = "";
+
+colorOptions = [
+  "wheat",
+  "coral",
+  "crimson",
+  "royalblue",
+  "violet",
+  "green",
+  "darkgreen",
+  "darkblue",
+  "indigo",
+  "chartreuse",
+  "aquamarine",
+  
+];
+
+function onSwitchColor() {
+  if (currentColor === "") {
+    currentColor = "brown";
+    applyColor(currentColor);
+    return;
+  }
+  const newColor = getANewColor(currentColor);
+  applyColor(newColor);
+}
+
+function applyColor(color) {
+  console.log("color is ", color);
+  const root = document.documentElement;
+  // set the primary color
+  root.style.setProperty('--primary-color', color);
+  // change the surname to the color
+  const surname = document.querySelector("#surname");
+  surname.textContent = color;
+
+}
+
+function getANewColor(currentColor) {
+  // return a random color that is not the current color
+  let newColor = currentColor;
+  while (newColor === currentColor) {
+    newColor = colorOptions[Math.floor(Math.random() * colorOptions.length)];
+  }
+  currentColor = newColor;
+  return newColor;
+}
